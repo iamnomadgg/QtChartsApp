61797610153f8dcd53e5e69d21a711d7dff54794
iamnomadgg <gokhangocmen94@gmail.com>
2023-02-03 19:48:03 +0100

    feat: add a chart and its elements.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,63 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtCharts/QChartView>
+#include <QtCharts/QBarSeries>
+#include <QtCharts/QBarSet>
+#include <QtCharts/QLegend>
+#include <QtCharts/QBarCategoryAxis>
+#include <QtCharts/QHorizontalStackedBarSeries>
+#include <QtCharts/QLineSeries>
+#include <QtCharts/QCategoryAxis>
+#include <QtCharts/QPieSeries>
+#include <QtCharts/QPieSlice>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QBarSet* set0 = new QBarSet("Altuve");
+    QBarSet* set1 = new QBarSet("Martinez");
+    QBarSet* set2 = new QBarSet("Segura");
+    QBarSet* set3 = new QBarSet("Simmons");
+    QBarSet* set4 = new QBarSet("Trout");
+
+    *set0 << 283 << 341 << 313 << 338 << 346 << 335;
+    *set1 << 250 << 315 << 282 << 307 << 303 << 330;
+    *set2 << 294 << 246 << 257 << 319 << 300 << 325;
+    *set3 << 248 << 244 << 265 << 281 << 278 << 313;
+    *set4 << 323 << 287 << 299 << 315 << 306 << 313;
+
+    QBarSeries* series = new QBarSeries();
+    series->append(set0);
+    series->append(set1);
+    series->append(set2);
+    series->append(set3);
+    series->append(set4);
+
+    QChart* chart = new QChart();
+    chart->addSeries(series);
+    chart->setTitle("Batting Avg by Year");
+    //No Animation, GridAxisAnimations, SeriesAnimations
+    chart->setAnimationOptions(QChart::AllAnimations);
+    QStringList categories;
+    categories << "2013" << "2014" << "2015" << "2016" << "2017" << "2018";
+    QBarCategoryAxis* axis = new QBarCategoryAxis();
+    axis->append(categories);
+    chart->createDefaultAxes();
+    //chart->setAxisX(axis, series);
+    chart->axes(Qt::Horizontal, series);
+    chart->legend()->setVisible(true);
+    chart->legend()->setAlignment(Qt::AlignBottom);
+
+    QChartView *chartView = new QChartView(chart);
+    chartView->setRenderHint(QPainter::Antialiasing);
+    QPalette pal = qApp->palette();
+    pal.setColor(QPalette::Window, QRgb(0xFFFFFF));
+    pal.setColor(QPalette::WindowText, QRgb(0x404040));
+    qApp->setPalette(pal);
+
     MainWindow w;
     w.show();
     return a.exec();
